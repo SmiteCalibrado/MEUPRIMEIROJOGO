daaca17e22729a66bed3e38c40145199d233469e
SmiteCalibrado <herogamer28@gmail.com>
2017-03-01 15:37:13 -0300

    the method isPrime was add

--- Assets/Scripts/inimigos.cs
+++ Assets/Scripts/inimigos.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class INIMIGOS : MonoBehaviour {
+	int count = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +12,23 @@ public class INIMIGOS : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+
+		isPrime (count);
+		count++;
+
 	}
+
+	private bool isPrime(int i){
+
+			if( i <= 1 ) 					return false;
+			if( i <= 3) 					return true;		
+			if ( i % 2 == 0 || i % 3 == 0 )	return false;
+
+			for(int x = 5; x*x <= i ; x = x + 6 ){
+				if(i % x == 0 || i % (x + 2) == 0) return false;
+			}
+
+			return true;
+		}
 }
