f4a7e23f1449cb044120370144287e2256d009d5
SmiteCalibrado <herogamer28@gmail.com>
2017-05-07 23:25:29 -0300

    Refazendo Tudo

---
+++ MEUPRIMEIROJOGO/Assets/Primo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AssemblyCSharp;
+using System;
+
+public class Primo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
