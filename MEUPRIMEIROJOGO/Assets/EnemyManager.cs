3f78ab25a3d09fa4437fc935d1f9fc3a92af3663
SmiteCalibrado <herogamer28@gmail.com>
2017-05-16 06:09:49 -0300

    gerador de inimigos

---
+++ MEUPRIMEIROJOGO/Assets/EnemyManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+	public GameObject enemy;    //Objeto onde inimigo Spawna
+	public float spawnTime = 3f; //Tempo De Spawn
+	public Transform[] spawnPoints;// Lugares do Spawn
+
+
+
+
+	void Start () {
+
+		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+		
+	}
+	
+
+	void Spawn () {
+
+		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+
+		Instantiate (enemy, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
+	}
+}
+
---
+++ MEUPRIMEIROJOGO/Assets/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	//Publicando Velocidade Maxima e fisica
+	public float speed;
+	public float maxSpeed;
+	private Rigidbody2D rigibodyPlayer;
+
+
+	// Use this for initialization
+	void Start () {
+		rigibodyPlayer = GetComponent<Rigidbody2D> ();  //Pegando Componente De Fisica
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//Adicionando velocidade para direcao X e Y
+		Vector2 inputDirectionX = new Vector2 (Input.GetAxis ("Horizontal") * speed, 0);
+		Vector2 inputDirectionY = new Vector2 (0, Input.GetAxis ("Vertical") * speed );
+
+		//Adicionando Forca para a direcao X
+		rigibodyPlayer.AddForce (inputDirectionX);
+
+		if (rigibodyPlayer.velocity.x > maxSpeed) {
+			rigibodyPlayer.velocity = new Vector2 (maxSpeed, rigibodyPlayer.velocity.y);
+		}
+		if (rigibodyPlayer.velocity.x < -maxSpeed) {
+			rigibodyPlayer.velocity = new Vector2 (-maxSpeed, rigibodyPlayer.velocity.y);
+
+		}
+		//Adicionando Forca para a direcao Y
+		rigibodyPlayer.AddForce (inputDirectionY);
+
+		if (rigibodyPlayer.velocity.y > maxSpeed) {
+			rigibodyPlayer.velocity = new Vector2 (rigibodyPlayer.velocity.x, maxSpeed);
+		}
+		if (rigibodyPlayer.velocity.y < -maxSpeed) {
+			rigibodyPlayer.velocity = new Vector2 (rigibodyPlayer.velocity.x, -maxSpeed);
+
+		}
+	}
+}
