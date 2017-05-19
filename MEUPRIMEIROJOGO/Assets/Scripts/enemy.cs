4bb2ffd597f67959e0c6ec823ece02ac8fa1c17a
SmiteCalibrado <herogamer28@gmail.com>
2017-05-18 23:36:39 -0300

    gerando inimigos parte 2

---
+++ MEUPRIMEIROJOGO/Assets/Scripts/enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+	private Rigidbody2D rigibodyEnemy;
+
+	// Use this for initialization
+	void Start () {
+		rigibodyEnemy = GetComponent<Rigidbody2D> ();  //Pegando Componente De Fisica
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		rigibodyEnemy.velocity = new Vector2 (rigibodyEnemy.velocity.x, -100);
+		
+	}
+}
